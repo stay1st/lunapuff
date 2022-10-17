77f9a63e5d40ece6bebee09911031f9f32766a70
Joshua Crawford <joshua.crawford.mail@gmail.com>
2022-10-17 15:17:56 -0400

    Working on Potfolio, practicing three.js in React

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,56 +1,59 @@
-import './index.scss';
-import React, { useState, useEffect } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
-import projects from "../../portfolio/portfolio.json"
-import cover from "../../assets/images/quote-generator.png"
-import AnimationThreeJS from '../AnimationThreeJS';
+import './index.scss'
+import React, { useState, useEffect, Suspense } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import AnimationThreeJS from '../AnimationThreeJS'
+import projects from '../../portfolio/portfolio.json'
+import cover from '../../assets/images/quote-generator.png'
 
 const Portfolio = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 2700);
-        return () => {
-            clearTimeout()
-        }
-    }, []);
-
-    const renderPortfolio = (projects) => {
-        return (
-            <div className='projects-container'>
-                {
-                    projects.map((port, idx) => {
-                        return (
-                            <div key={'project-' + idx.toString()} className='project-covers-container'>
-                                <img
-                                    className='projects-img'
-                                    src={cover}
-                                    alt='Quote-Generator-Mobile-UI'
-                                />
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 2700)
+    return () => {
+      clearTimeout()
     }
+  }, [])
 
+  const renderPortfolio = (projects) => {
     return (
-        <div className='container portfolio-page'>
-            <h1 className='page-title'>
-                <AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']}
-                    idx={15}
-                />
-            </h1>
-            <div>{renderPortfolio(projects.portfolio)}</div>
-            <canvas id='webgl-port'></canvas>
-            <AnimationThreeJS />
-        </div>
+      <div className="projects-container">
+        {projects.map((port, idx) => {
+          return (
+            <div
+              key={'project-' + idx.toString()}
+              className="project-covers-container"
+            >
+              <img
+                className="projects-img"
+                src={cover}
+                alt="Quote-Generator-Mobile-UI"
+              />
+            </div>
+          )
+        })}
+      </div>
     )
+  }
+
+  return (
+    <div className="container portfolio-page">
+      <h1 className="page-title">
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={['P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']}
+          idx={15}
+        />
+      </h1>
+      <Suspense>
+        <AnimationThreeJS 
+            portImages={renderPortfolio(projects.portfolio)}
+            className='threejs-canvas' />
+      </Suspense>
+    </div>
+  )
 }
 
-export default Portfolio;
+export default Portfolio
